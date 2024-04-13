b96257d8ac71566299b9112c65de28534f5b89e0
rwkliu <114320485+rwkliu@users.noreply.github.com>
2024-04-13 08:27:41 -0700

    My Scenario 3
    This simulation hits every GET and POST request. The injection profile ramps the users/sec by 1 up to 500 for 30 seconds, for a maximum duration of 1 minute.

--- src/test/java/warriordatabase/warriorDatabaseSimulation.java
+++ src/test/java/warriordatabase/warriorDatabaseSimulation.java
@@ -7,6 +7,7 @@ import io.gatling.core.scenario.Scenario;
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
 
+import java.time.Duration;
 import java.util.concurrent.ThreadLocalRandom;
 public class warriorDatabaseSimulation extends Simulation {
     // Add the HttpProtocolBuilder
@@ -17,17 +18,31 @@ public class warriorDatabaseSimulation extends Simulation {
     // Add the ScenarioBuilder
     ScenarioBuilder myScenario = scenario("My Scenario")
         .exec(
-            http("Landing Page").get("/"),
-            pause(2),
             http("Find Neo")
                 .get("warrior?t=Neo")
+                .check(status().is(200)),
+            pause(1),
+            http("Add new warrior")
+                .post("/warrior")
+                .body(StringBody("{\"id\": \"196d5c01-6aae-49aa-a7cd-c525198933dc\",\n" +
+                    "    \"name\": \"Agent Smith\",\n" +
+                    "    \"dob\": \"1970-01-01\",\n" +
+                    "    \"fight_skills\": [\"KungFu\", \"Karate\", \"Hapkido\"] }"))
+                .check(status().is(201)),
+            pause(1),
+            http("Find Neo by id")
+                .get("warrior/3b323968-73d8-47a3-a84d-9db948e2bd6b")
+                .check(status().is(200)),
+            pause(1),
+            http("Count Warriors")
+                .get("/counting-warriors")
                 .check(status().is(200))
         );
 
     // Add the setUp block
     {
         setUp(
-            myScenario.injectOpen(rampUsersPerSec(50).to(1500).during(30))
-        ).protocols(httpProtocol);
+            myScenario.injectOpen(rampUsersPerSec(1).to(500).during(30))
+        ).protocols(httpProtocol).maxDuration(Duration.ofMinutes(1));
     }
 }
